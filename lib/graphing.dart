ef060ffb21c8612b4cf68b4d160a8fb81ebf93e1
Trent Piercy <mcpvpxie@gmail.com>
2018-04-08 22:11:31 -0500

    optimize OHLC graphing

--- lib/graphing.dart
+++ lib/graphing.dart
@@ -1,6 +1,3 @@
-import 'dart:math' as math;
-import 'dart:ui' as ui show PointMode;
-
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
@@ -38,10 +35,7 @@ class OHLCVGraph extends StatelessWidget {
 
 class _OHLCVPainter extends CustomPainter {
 
-  _OHLCVPainter(
-      this.data, {
-        @required this.lineWidth
-      });
+  _OHLCVPainter(this.data, {@required this.lineWidth});
 
   final List data;
   final double lineWidth;
@@ -52,7 +46,6 @@ class _OHLCVPainter extends CustomPainter {
 
     double _min = double.infinity;
     double _max = 0.0;
-
     for (var i in data) {
       if (i["high"] > _max) {
         _max = i["high"];
@@ -64,43 +57,43 @@ class _OHLCVPainter extends CustomPainter {
 
     final double width = size.width;
     final double height = size.height;
-
     final double heightNormalizer = height / (_max - _min);
-
     final double rectWidth = width / data.length;
 
+    double rectLeft;
+    double rectTop;
+    double rectRight;
+    double rectBottom;
+
+    Paint rectPaint;
+
     for (int i = 0; i < data.length; i++) {
+      rectLeft = (i * rectWidth) + lineWidth/2;
+      rectRight = ((i+1) * rectWidth) - lineWidth/2;
 
       if (data[i]["open"] > data[i]["close"]) {
-        double rectLeft = i * rectWidth;
-        double rectRight = (i+1) * rectWidth;
-        double rectTop = height - (data[i]["open"] - _min) * heightNormalizer;
-        double rectBottom = height - (data[i]["close"] - _min) * heightNormalizer;
-        Paint rectPaint = new Paint() ..color=Colors.red ..strokeWidth=lineWidth;
+        rectTop = height - (data[i]["open"] - _min) * heightNormalizer;
+        rectBottom = height - (data[i]["close"] - _min) * heightNormalizer;
+        rectPaint = new Paint() ..color=Colors.red ..strokeWidth=lineWidth;
+
         Rect ocRect = new Rect.fromLTRB(rectLeft, rectTop, rectRight, rectBottom);
         canvas.drawRect(ocRect, rectPaint);
 
-        double low = height - (data[i]["low"] - _min) * heightNormalizer;
-        double high = height - (data[i]["high"] - _min) * heightNormalizer;
-        canvas.drawLine(new Offset(rectLeft+rectWidth/2-lineWidth/2, rectBottom), new Offset(rectLeft+rectWidth/2-lineWidth/2, low), rectPaint);
-        canvas.drawLine(new Offset(rectLeft+rectWidth/2-lineWidth/2, rectTop), new Offset(rectLeft+rectWidth/2-lineWidth/2, high), rectPaint);
-
       } else {
-        double rectLeft = (i * rectWidth) + lineWidth/2;
-        double rectRight = ((i+1) * rectWidth) - lineWidth/2;
-        double rectTop = (height - (data[i]["close"] - _min) * heightNormalizer) + lineWidth/2;
-        double rectBottom = (height - (data[i]["open"] - _min) * heightNormalizer) - lineWidth/2;
-        Paint rectPaint = new Paint() ..color=Colors.green ..strokeWidth=lineWidth;
+        rectTop = (height - (data[i]["close"] - _min) * heightNormalizer) + lineWidth/2;
+        rectBottom = (height - (data[i]["open"] - _min) * heightNormalizer) - lineWidth/2;
+        rectPaint = new Paint() ..color=Colors.green ..strokeWidth=lineWidth;
+
         canvas.drawLine(new Offset(rectLeft-lineWidth/2, rectBottom), new Offset(rectRight+lineWidth/2, rectBottom), rectPaint);
         canvas.drawLine(new Offset(rectLeft-lineWidth/2, rectTop), new Offset(rectRight+lineWidth/2, rectTop), rectPaint);
         canvas.drawLine(new Offset(rectLeft, rectBottom), new Offset(rectLeft, rectTop), rectPaint);
         canvas.drawLine(new Offset(rectRight, rectBottom), new Offset(rectRight, rectTop), rectPaint);
-
-        double low = height - (data[i]["low"] - _min) * heightNormalizer;
-        double high = height - (data[i]["high"] - _min) * heightNormalizer;
-        canvas.drawLine(new Offset(rectLeft+rectWidth/2-lineWidth/2, rectBottom), new Offset(rectLeft+rectWidth/2-lineWidth/2, low), rectPaint);
-        canvas.drawLine(new Offset(rectLeft+rectWidth/2-lineWidth/2, rectTop), new Offset(rectLeft+rectWidth/2-lineWidth/2, high), rectPaint);
       }
+
+      double low = height - (data[i]["low"] - _min) * heightNormalizer;
+      double high = height - (data[i]["high"] - _min) * heightNormalizer;
+      canvas.drawLine(new Offset(rectLeft+rectWidth/2-lineWidth/2, rectBottom), new Offset(rectLeft+rectWidth/2-lineWidth/2, low), rectPaint);
+      canvas.drawLine(new Offset(rectLeft+rectWidth/2-lineWidth/2, rectTop), new Offset(rectLeft+rectWidth/2-lineWidth/2, high), rectPaint);
 
     }
 
